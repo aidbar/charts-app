3531b29d5adab89e872fd9eb7097b0700d341ec7
aidbar <47143229+aidbar@users.noreply.github.com>
2024-08-07 18:49:50 +0300

    initial commit (routing)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ViewModeComponent } from './view-mode/view-mode.component';
+import { SettingsComponent } from './settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'view-mode', component: ViewModeComponent},
+    {path: 'settings', component: SettingsComponent},
+    {path: '', redirectTo: '/view-mode', pathMatch: 'full'}
+];
